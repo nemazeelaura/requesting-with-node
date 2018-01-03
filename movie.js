32f116991b56ee6ed13ef4a3064cb1f108ce686d
nemazeelaura <arvadajohnson@gmail.com>
2018-01-02 17:40:52 -0700

    adding movie.js file

--- movie.js
+++ movie.js
@@ -5,12 +5,17 @@
 const request = require('./node_modules/request');
 const keys = require('./env');
 
+
+ // console.log(keys);
 function getMovieTitle(title){
-    request('https://www.googleeapis.com/customsearch/v1?key='+keys.key+ '&cx='+keys.id+ title + '', function (error, response, body) {
+    request('https://www.googleapis.com/customsearch/v1?key=' + keys.key + '&cx='+ keys.id + '&q=' + title + '', function (error, response, body) {
     // call back
     var info = JSON.parse(body);
     console.log(info.items[0].title);
+    // console.log(response);
+    // console.log(error);
     //print out movies
+    // 
   });
     
 }
